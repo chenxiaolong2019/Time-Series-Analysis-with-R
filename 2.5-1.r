c62f177521dceeb696c58598fb10f4333172278d
chenxiaolong2019 <64651771+chenxiaolong2019@users.noreply.github.com>
2020-10-21 19:58:24 +0800

    Create 2.5-1.r

---
+++ 2.5-1.r
@@ -0,0 +1,11 @@
+> x<-c(1:20)
+> for(k in 1:6){r=acf(x,lag=k)}
+> r
+
+Autocorrelations of series ‘x’, by lag
+
+    0     1     2     3     4     5     6 
+1.000 0.850 0.702 0.556 0.415 0.280 0.153 
+
+> acf(x,lag=)
+> 
