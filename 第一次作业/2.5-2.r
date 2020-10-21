d4a4bf8da58ddd8b888de20cd725d80bd36e1999
chenxiaolong2019 <64651771+chenxiaolong2019@users.noreply.github.com>
2020-10-21 21:18:57 +0800

    Update 2.5-2.r

--- 第一次作业/2.5-2.r
+++ 第一次作业/2.5-2.r
@@ -1,5 +1,5 @@
-a<-read.table("C:/Users/35865/Desktop/时间序列分析/习题2.2.csv",sep=",",header=T)
-co2<-ts(a,start=c(1975,1),frequency=12)
+> a<-read.table("C:/Users/35865/Desktop/时间序列分析/习题2.2.csv",sep=",",header=T)
+> co2<-ts(a,start=c(1975,1),frequency=12)
 
 #序列时序图
 > plot(co2,main="1975年-1980年夏威夷Munna Loa火山每月释放的CO2数据",xlab="年份",ylab="CO2 单位:ppm")
